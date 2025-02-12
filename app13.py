58f8aeb30718b7868b6c66dbbf4d3db3fb06e2cd
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-02-12 13:08:05 -0600

    Update app13.py

--- app13.py
+++ app13.py
@@ -12,6 +12,25 @@ import plotly.express as px
 # Configuración de la página
 st.set_page_config(page_title="Análisis de Movilidad Laboral", page_icon="📊", layout="wide")
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+
+
 # Título de la aplicación
 st.title("Análisis de Movilidad Laboral y su Impacto en Pensiones")
 
